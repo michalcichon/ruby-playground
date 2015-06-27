3ea79c9f1dfd8f10604bc17a5cc1daa826e506fa
Michał Cichoń <michal.cichon@aol.com>
2015-06-27 19:35:52 +0200

    array

--- array.rb
+++ array.rb
@@ -3,3 +3,19 @@ puts "First element: #{a[0]}"
 a[1] = nil
 puts "Array: #{a}"
 puts "Array inspect: #{a.inspect}"
+
+h = {
+  'name' => 'Michal',
+  'surname' => 'Cichon',
+  'age' => 27
+}
+
+puts "h.name: #{h['name']}"
+puts "h: #{h}"
+
+h2 = Hash.new(0)
+puts h2
+puts h2['not_defined']
+# h2['a']++
+h2['a'] += 1
+puts h2['a']
