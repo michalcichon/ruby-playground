4e6d8c33613c9c5e65fd198edf273fcf9abfe71c
Michał Cichoń <michal.cichon@aol.com>
2015-06-27 19:58:02 +0200

    array: hash with symbols

--- array.rb
+++ array.rb
@@ -19,3 +19,16 @@ puts h2['not_defined']
 # h2['a']++
 h2['a'] += 1
 puts h2['a']
+
+h3 = {
+  :symbol1 => 'value1',
+  :symbol2 => 'value2'
+}
+
+h3_short = {
+  symbol1: 'value1',
+  symbol2: 'value2'
+}
+
+puts 'h3[:symbol1]: #{h3[:symbol1]}'
+puts 'h3_short[:symbol1]: #{h3_short[:symbol1]}'
