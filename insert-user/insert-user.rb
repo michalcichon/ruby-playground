ac624d2fa13ad3ddfb29b2b065be3ca68744c779
Michał Cichoń <michal.cichon@aol.com>
2015-06-28 15:04:23 +0200

    insert-user get filename from argv

--- insert-user/insert-user.rb
+++ insert-user/insert-user.rb
@@ -13,6 +13,18 @@ def create_insert_user(user)
   return "INSERT INTO users (firstname, surname, email, password) VALUES ('#{user[:firstname]}', '#{user[:lastname]}', '#{user[:email]}', '#{user[:password]}')"
 end
 
-CSV.foreach('users.csv', {col_sep:';'}) do |user|
+if ARGV.empty?
+  puts 'Usage: ruby insert-ruby.rb filename';
+  exit
+end
+
+filename = ARGV[0]
+
+unless File.exist?(filename)
+  puts "File #{filename} is not exist."
+  exit
+end
+
+CSV.foreach(filename, {col_sep:';'}) do |user|
   puts create_insert_user(user_hash(user))
 end
