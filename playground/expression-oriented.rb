16198843c0d07bb54bc6a28d5a72d84d52240159
Michal Cichon <michal.cichon@aol.com>
2016-03-01 22:45:24 +0100

    expression oriented

---
+++ playground/expression-oriented.rb
@@ -0,0 +1,12 @@
+# Ruby is expression-oriented language
+x = 5
+y = 2
+minimum = if x < y then x else y end
+puts minimum
+
+# return statement is not necessary
+def square(x)
+  x*x
+end
+
+puts square(x)
